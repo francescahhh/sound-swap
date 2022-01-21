10409f2d4dcf5c92cc3a4d1b6ee941651f4de988
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-20 22:40:05 -0700

    added components

---
+++ client/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const header = () => {
+return (
+    <header className="Main-header">
+        <h1 id="page-title">SoundSwap</h1>
+    </header>
+)
+}
+
+export default header
