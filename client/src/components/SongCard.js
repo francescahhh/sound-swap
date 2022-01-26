6edcd033c8f5f3b19f384f2debfed6afb9c83ead
Jake <jakerfilut@gmail.com>
2022-01-25 23:22:18 -0700

    updated search

--- client/src/components/SongCard.js
+++ client/src/components/SongCard.js
@@ -1,12 +1,34 @@
 import React from "react";
 
 function SongCard({ results }) {
+  const convertHMS = (value) => {
+    const sec = parseInt(value, 10); // convert value to number if it's string
+    let hours = Math.floor(sec / 3600); // get hours
+    let minutes = Math.floor((sec - hours * 3600) / 60); // get minutes
+    let seconds = sec - hours * 3600 - minutes * 60; //  get seconds
+    // add 0 if value < 10; Example: 2 => 02
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+    console.log(hours + ":" + minutes + ":" + seconds);
+    return minutes + ":" + seconds; // Return is HH : MM : SS
+  };
+
   const mapResults = results.map((song) => {
     console.log(song);
     return (
       <div className="song_container_card">
-        <div>{song.name}</div>
-        <img src={song.image[2]} />
+        <h1>{song.title}</h1>
+        <img src={song.album.cover} />
+        <div>{song.artist.name}</div>
+        <div>{convertHMS(song.duration)}</div>
+        <button>Add to Playlist</button>
       </div>
     );
   });
